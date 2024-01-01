4ca53487434f77f9521e06fc5f622846fea79a1c
Mukesh kachhawah <mukeshkachhawah1947@gmail.com>
2024-01-01 23:40:26 +0530

    Happy new year starting challenging

--- src/Scenerio Based Question/Javascript-Scenerio-Base-Question/Js.js
+++ src/Scenerio Based Question/Javascript-Scenerio-Base-Question/Js.js
@@ -1,3 +1,8 @@
-const nestArray = [1, 2, 3, [4, 5, 6, [7, 8, 9], [10, 11, 12]]];
-const flatedArray = nestArray.flat(Infinity);
-console.log(flatedArray);
+//Class and method define in js
+//class inheritance in js
+//find duplicate in array using for loop
+//find the count of the duplcate element
+//how to check given number is integer or not in js
+//difference between Object.freeze() and const in js
+//sort a array in js
+//find the unique element in array using for loop
