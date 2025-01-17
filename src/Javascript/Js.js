9683ef70a10fd3b3cb10ceac7ca5fc5020f7dbd1
Mukesh kachhawah <mukeshkachhawah1947@gmail.com>
2025-01-17 07:57:14 +0530

    Scenario: Implementing a Custom Promise

--- src/Javascript/Js.js
+++ src/Javascript/Js.js
@@ -1,7 +1,41 @@
-for (var i = 1; i <= 5; i++) {
-  (function (i) {
-    setTimeout(() => {
-      console.log(i);
-    }, i * 1000);
-  })(i);
+class CustomPromise {
+  constructor(executor) {
+    this.onResolve = null;
+    this.onReject = null;
+
+    const resolve = (value) => {
+      if (this.onResolve) {
+        this.onResolve(value);
+      }
+    };
+
+    const reject = (error) => {
+      if (this.onReject) {
+        this.onReject(error);
+      }
+    };
+
+    executor(resolve, reject);
+  }
+
+  then(callback) {
+    this.onResolve = callback;
+    return this;
+  }
+
+  catch(callback) {
+    this.onReject = callback;
+    return this;
+  }
 }
+
+const promise = new CustomPromise((resolve) => {
+  setTimeout(() => resolve("Resolved after 2 seconds"), 2000);
+});
+
+promise.then((value) => {
+  console.log(value);
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+});
