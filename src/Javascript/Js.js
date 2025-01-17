93a3c4895d1987cedaffa4b0f0deaa6dcb3245ea
Mukesh kachhawah <mukeshkachhawah1947@gmail.com>
2025-01-17 08:05:16 +0530

    how can you create a immutable object does not add delete and change the value ?

--- src/Javascript/Js.js
+++ src/Javascript/Js.js
@@ -1,41 +1,6 @@
-class CustomPromise {
-  constructor(executor) {
-    this.onResolve = null;
-    this.onReject = null;
-
-    const resolve = (value) => {
-      if (this.onResolve) {
-        this.onResolve(value);
-      }
-    };
-
-    const reject = (error) => {
-      if (this.onReject) {
-        this.onReject(error);
-      }
-    };
-
-    executor(resolve, reject);
-  }
-
-  then(callback) {
-    this.onResolve = callback;
-    return this;
-  }
-
-  catch(callback) {
-    this.onReject = callback;
-    return this;
-  }
-}
-
-const promise = new CustomPromise((resolve) => {
-  setTimeout(() => resolve("Resolved after 2 seconds"), 2000);
+const obj = Object.freeze({
+  name: "Mukks",
+  age: 22,
+  city: "jaipur",
 });
-
-promise.then((value) => {
-  console.log(value);
-  for (let i = 1; i <= 10; i++) {
-    console.log(i);
-  }
-});
+console.log(obj.name);
